35ca38eee106fe18a6b0b140fed9991ce14a81b8
Ulises Ruiz <supertepo@outlook.com>
2023-02-01 11:10:12 -0600

    botones selectores

---
+++ resources/script.js
@@ -0,0 +1,29 @@
+const button = document.getElementById("all");
+const button22 = document.getElementById("22");
+const button21 = document.getElementById("21");
+const divs = document.querySelectorAll(".proyectos");
+
+button.addEventListener("click", function() {
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].style.display = "block";
+  }
+  button22.classList.remove("active");
+  button21.classList.remove("active");
+  button.classList.toggle("active");
+});
+button22.addEventListener("click", function() {
+  for (let i = 0; i < divs.length; i++) {
+    if (divs[i].parentNode.id !== "2022") {
+      divs[i].style.display = divs[i].style.display === "none" ? "block" : "none";
+    }
+  }
+  button22.classList.toggle("active");
+});
+button21.addEventListener("click", function() {
+  for (let i = 0; i < divs.length; i++) {
+    if (divs[i].parentNode.id !== "2021") {
+      divs[i].style.display = divs[i].style.display === "none" ? "block" : "none";
+    }
+  }
+  button21.classList.toggle("active");
+});
